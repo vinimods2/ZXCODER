d2a9e0bbf4620399107a6211089b39ac3a93376b
Ichanxd <klompokalmas07@gmail.com>
2022-10-22 11:09:41 +0700

    klompokalmas07@gmail.com

---
+++ setting.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.Api = {
+	ampibi: 'https://ampibismm.my.id',
+}
+
+// Apikey Website Api
+global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
+
+global.smm = {
+  nomor : '6283102650464',
+  atasnama : 'Ahmad Nur Fadhil'
+}
+
+// Other
+global.owner = ['6282334018401',"6282164826318"]
+global.premium = ['6282334018401']
+global.sponsor = 'no sponsors in:('
+global.packname = 'ZXCoderID'
+global.footer = '© 2022 IchanZX ID'
+global.author = 'Owner : IchanZX Coder-ID\nYT : IchanGaming'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.act = {
+		wait: 'please wait a moment, processing data...',
+		den:'*Sorry You Not Register Yet...*\n\n*.reg* <name>\n*To Regis/Login*',
+		proc:'Loading Please Wait..',
+		error:'Error :(',
+		success:'Success bang!',
+		repiar:'This Feature Maintace!',
+		botadm:'Jadiin gw admin dulu!',
+		admin:'Khusus admin woi!',
+		done:'Done!',
+		gc:'Only Group Allowed',
+		owner:'Only Owner Allowed'
+}
+
+global.thumb = fs.readFileSync('./lib/crizzy.jpg')
+global.crizzy = { url: "https://g.top4top.io/p_2443v88g61.jpg"}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
